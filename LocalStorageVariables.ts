d8bdf62cf0e1dd95c8668d4ebf7ff186a16ce5a8
Daniel Leitch <40886941+danleitch@users.noreply.github.com>
2022-04-03 12:13:29 +0200

    Making it more generic

--- LocalStorageVariables.ts
+++ LocalStorageVariables.ts
@@ -1,12 +1,11 @@
-import { SignInData } from '../../store/auth/helpers'
+
 
 class LocalStorageVariables {
-  private static key: string = 'lakdnHRU'
+  private static key: string = 'string'
 
-  data: SignInData
 
-  static get(): SignInData {
-    const data = globalThis.localStorage?.getItem(this.key) || null
+  static get() {
+    const data = localStorage?.getItem(this.key) || null
 
     if (!data) {
       throw new Error('Error: Data is not defined in storage.')
@@ -15,12 +14,12 @@ class LocalStorageVariables {
     return JSON.parse(data)
   }
 
-  static set(data: SignInData): void {
-    globalThis.localStorage?.setItem(this.key, JSON.stringify(data))
+  static set(data): void {
+    localStorage?.setItem(this.key, JSON.stringify(data))
   }
 
   static remove(): void {
-    globalThis.localStorage?.removeItem(this.key)
+    localStorage?.removeItem(this.key)
   }
 }
 
